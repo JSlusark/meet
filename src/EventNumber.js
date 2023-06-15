3db686576c75a5d54c7dfb5dc79c22d8755ae672
Jessica Slusark <slusark.jessica@gmail.com>
2023-06-15 15:21:57 +0200

    Added component for unit Testing via Jest

---
+++ src/EventNumber.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+class EventNumber extends Component {
+	state = {
+		query: 32,
+	};
+
+	handleInputChanged = (event) => {
+		let value = event.target.value;
+		this.props.updateEvents(null, value);
+		this.setState({ query: value });
+	};
+
+	render() {
+		return (
+			<div className="number-of-events">
+				<label className="number-of-events-label">Number of Events: </label>
+				<input
+					type="number"
+					className="number-of-events-input"
+					value={this.state.query}
+					onChange={this.handleInputChanged}
+				/>
+			</div>
+		);
+	}
+}
+
+export default EventNumber;
