0ef5630a762abcf13a18b706193f9581304f7433
Jessica Slusark <slusark.jessica@gmail.com>
2023-06-22 00:03:24 +0200

    Setting up Atatus for performance  monitoring

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
+import * as atatus from "atatus-spa";
+atatus.config("07617f677b0c4d769b0a7c576ba20e71").install();
 
 ReactDOM.render(
 	<React.StrictMode>
